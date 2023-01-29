e9af325a0895f4091a0ec6570a35c0fc44c93724
Mahziyar Gilanpour <mgillanpour79@gmail.com>
2023-01-29 18:35:49 +0330

    start car carts

---
+++ components/CarCart.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { HeartIcon } from "@heroicons/react/24/outline";
+
+function CarCart() {
+  return (
+    <>
+        <div className="bg-white p-4">
+            <section className="flex items-start justify-between">
+                <div>
+                  <h4>Koenigsegg</h4>
+                  <span>Sport</span>
+                </div>
+                <HeartIcon className="scroll-effect h-6 w-6" />
+            </section>
+        </div>
+        <div className="bg-white h-20 w-20">dasd</div>
+        <div className="bg-white h-20 w-20">dasd</div>
+        <div className="bg-white h-20 w-20">dasd</div>
+        <div className="bg-white h-20 w-20">dasd</div>
+        <div className="bg-white h-20 w-20">dasd</div>
+        <div className="bg-white h-20 w-20">dasd</div>
+        <div className="bg-white h-20 w-20">dasd</div>
+    </>
+  );
+}
+
+export default CarCart;
