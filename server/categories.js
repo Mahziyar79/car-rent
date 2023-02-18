0445ab8e2412932acc028b661687c06941871b79
Mahziyar Gilanpour <mgillanpour79@gmail.com>
2023-02-18 09:30:25 +0330

    back category file

---
+++ server/categories.js
@@ -0,0 +1,22 @@
+export const categories = [
+  {
+    id: 1,
+    category: "Sport",
+    quantity: "2",
+  },
+  {
+    id: 2,
+    category: "Sedan",
+    quantity: "1",
+  },
+  {
+    id: 3,
+    category: "SUV",
+    quantity: "4",
+  },
+  {
+    id: 4,
+    category: "Hatchback",
+    quantity: "1",
+  },
+];
